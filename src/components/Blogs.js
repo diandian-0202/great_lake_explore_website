8cfb040fbaf7efc7be47c73e1ce36741353ceec0
John Oyer <johnoyer@umich.edu>
2024-11-20 13:37:45 -0500

    devblog 2

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -27,8 +27,27 @@ function Blogs({ scrollPositionHeader}) {
                 mechanics to the world, and created a boat that the user can ride.
                 </p>
             </div>
+        </div>
+        <div>
+            <div className="content-box">
+                <h2 className="dev-blog-header">Dev Blog 11/13 - 11/20: Second Week</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/hhkCj4we0bE?si=6PIzMmrsmL1iW4kX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p className="dev-blog-text">
+                In our second week, we aimed to develop our experience to the point where the 
+                user would have a stronger sense of the environment of the game and the 
+                purpose of the game.
+                </p>
+                <p className="dev-blog-text">
+                We selected the specific animals that we would like to represent in the game 
+                as well as made the board representing the food chain with all the connections 
+                between the the animals. We also added rain to our world, and enhanced the 
+                landscape through modeling and the addition of trees. The user also has the ability 
+                to teleport between lakes, and information appears when the user 
+                catches a fish.
+                </p>
             </div>
         </div>
+    </div>
     )
 }
 
